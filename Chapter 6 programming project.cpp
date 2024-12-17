5c1f6bc03265aeff6802f7f24f90c596c2c4f1a8
careybarberjr@gmail.com <careybarberjr@gmail.com>
2024-12-17 09:51:38 -0500

    a

--- Chapter 6 programming project.cpp
+++ Chapter 6 programming project.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-double celciusconvers(double celcius, double fahrenheit);
+double celciusconvers(double celcius, double fahrenheit, double fraction);
 
 int main()
 {
-	double celcius = 0, fahrenheit = 0;
+	double celcius= 10, fahrenheit = 0, fraction = 0.55;
 
-	cout << "please enter your desired temperature in fahreneit" << endl;
-	cin >> fahrenheit;
+	while (fahrenheit <= 0 || fahrenheit >= 20)
+	{
+		cout << "please enter your desired temperature in fahreneit" << endl;
+		cin >> fahrenheit;
+		fahrenheit = fahrenheit * 1.0;
+		if (fahrenheit <= 0 || fahrenheit >= 20)
+		{
+			cout << "try again, has to be between 0 and 20" << endl;
+		}
+	}
+	celcius = celciusconvers(celcius, fahrenheit, fraction);
 
 	cout << fahrenheit << " degrees fahrenheit is " << celcius << " degrees celcius." << endl;
 
 }
 
-double celciusconvers (double celcius, double fahrenheit)
+double celciusconvers (double celcius, double fahrenheit, double fraction)
 {
-	celcius = (5/9)*(fahrenheit - 32);
-	return celcius;
+
+	return celcius = (fraction)*(fahrenheit - 32);
 
 }  
